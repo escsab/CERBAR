b70ab47e3f2452aa9cb88f7ad02f10f95bfd6389
escsab <rafa.sabater@gmail.com>
2014-01-28 22:23:33 +0100

    Cerbar 0.1
    Primera versió

---
+++ test/CDR/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-10-16 17:24:14 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-10-16T17:24:14+00:00	INFO	joomlafailure	Empty password not allowed
+2013-10-16T17:24:20+00:00	INFO	joomlafailure	Empty password not allowed
+2013-10-27T09:55:55+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
